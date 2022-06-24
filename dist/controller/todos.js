4f588e6ee1a350957dce3769c11792675fad0681
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2022-06-24 22:57:05 +0100

    worked on the create todo controller

--- dist/controller/todos.js
+++ dist/controller/todos.js
@@ -1,5 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createTodo = void 0;
-const createTodo = (req, res, next) => { };
+const todo_1 = require("../models/todo");
+const TODOS = [];
+const createTodo = (req, res, next) => {
+    var _a;
+    const text = (_a = req === null || req === void 0 ? void 0 : req.body) === null || _a === void 0 ? void 0 : _a.text;
+    const newTodo = new todo_1.Todo(Math.random().toString(), text);
+    TODOS.push(newTodo);
+};
 exports.createTodo = createTodo;
--- src/controller/todos.ts
+++ src/controller/todos.ts
@@ -1,3 +1,11 @@
 import { RequestHandler } from "express";
+import { Todo } from "../models/todo";
 
-export const createTodo: RequestHandler = (req, res, next) => {};
+const TODOS: Todo[] = [];
+
+export const createTodo: RequestHandler = (req, res, next) => {
+  const text = (req?.body as { text: string })?.text;
+  const newTodo = new Todo(Math.random().toString(), text);
+
+  TODOS.push(newTodo);
+};
