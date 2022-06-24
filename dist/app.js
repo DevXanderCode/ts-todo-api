0e78c0a6f81070a114c5392df06cfd3ccbed1787
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2022-06-24 22:43:56 +0100

    created an error handling middleware

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,9 @@ const express_1 = __importDefault(require("express"));
 const todos_1 = __importDefault(require("./routes/todos"));
 const app = (0, express_1.default)();
 app.use("/todos", todos_1.default);
-app.use((err, req, res, next) => { });
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: err === null || err === void 0 ? void 0 : err.message });
+});
 app.listen(3000, () => {
     console.log("app listening on port 3000");
 });
--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,9 @@ const app = express();
 
 app.use("/todos", todoRoutes);
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {});
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err?.message });
+});
 
 app.listen(3000, () => {
   console.log("app listening on port 3000");
